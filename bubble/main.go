49bd1a5cf87bada6ee6b6ffb7c1ef9aaa25b7467
Youndge <18610457408@163.com>
2021-03-24 21:59:10 +0800

    init myGin Program

---
+++ bubble/main.go
@@ -0,0 +1,50 @@
+package main
+
+import(
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+type Todo struct{
+	ID int `json:"id"`
+	Title string `json:"title"`
+	Status string `json:"status"`
+}
+
+func main()  {
+	
+
+	r := gin.Default()
+	r.Static("/static","static")
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/",func(c *gin.Context)  {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	v1Group := r.Group("v1")
+	{
+		//代办事项
+		//ADD
+		v1Group.POST("/todo",func(c *gin.Context)  {
+			
+		})
+		//查看所有代办事项
+		v1Group.GET("/todo",func(c *gin.Context)  {
+			
+		})
+		//查看某一个代办事项
+		v1Group.GET("/todo:id",func(c *gin.Context)  {
+			
+		})
+		//查看所有代办事项
+		v1Group.PUT("/todo:id",func(c *gin.Context)  {
+			
+		})
+		//查看所有代办事项
+		v1Group.DELETE("/todo:id",func(c *gin.Context)  {
+			
+		})
+
+	}
+
+	r.Run()
+}
