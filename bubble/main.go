5fdb7a0f87ac2d485f7748d8d60c84f2754af0e9
马靖玮 <majingwei@majingweideMBP.lan>
2021-03-28 17:01:30 +0800

    lesson 26 complete 2 finish

--- bubble/main.go
+++ bubble/main.go
@@ -12,10 +12,11 @@ var (
 	DB *gorm.DB
 )
 
+// Todo Model
 type Todo struct{
 	ID int `json:"id"`
 	Title string `json:"title"`
-	Status string `json:"status"`
+	Status bool `json:"status"`
 }
 
 func initMySQL() (err error){
@@ -30,10 +31,10 @@ func initMySQL() (err error){
 	return
 }
 
-func main()  {
-	//创建数据库
-	//sql:CREATE DATEBASE bubble;
-	//链接数据库
+func main() {
+	// 创建数据库
+	// sql: CREATE DATABASE bubble;
+	// 连接数据库
 	err := initMySQL()
 	if err != nil{
 		panic(err)
@@ -44,57 +45,65 @@ func main()  {
 
 
 	r := gin.Default()
-	r.Static("/static","static")
+	// 告诉gin框架模板文件引用的静态文件去哪里找
+	r.Static("/static", "static")
+	// 告诉gin框架去哪里找模板文件
 	r.LoadHTMLGlob("templates/*")
 	r.GET("/",func(c *gin.Context)  {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
-
+	
+	// v1
 	v1Group := r.Group("v1")
 	{
-		//代办事项
-		//ADD
-		v1Group.POST("/todo",func(c *gin.Context)  {
-			//前端页面填写待办事项，点击提交，会发送请求到这里
-			//1.从请求中把数据取出来
+		// 待办事项
+		// 添加
+		v1Group.POST("/todo", func(c *gin.Context) {
+			// 前端页面填写待办事项 点击提交 会发请求到这里
+			// 1. 从请求中把数据拿出来
 			var todo Todo
 			c.BindJSON(&todo)
-			//2.存入数据库
-			//DB.Create(&todo)
-			//3.返回响应
-			if err = DB.Create(&todo).Error;err != nil{
-				c.JSON(http.StatusOK, gin.H{"error":err.Error()})
+			// 2. 存入数据库
+			//err = DB.Create(&todo).Error
+			//if err!= nil {
+			//}
+			// 3. 反回响应
+			if err = DB.Create(&todo).Error;err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
 			}else{
-				c.JSON(http.StatusOK,todo)
+				c.JSON(http.StatusOK, todo)
+				//c.JSON(http.StatusOK, gin.H{
+				//	"code": 2000,
+				//	"msg": "success",
+				//	"data": todo,
+				//})
 			}
 		})
-		//查看所有代办事项
-		v1Group.GET("/todo",func(c *gin.Context)  {
-			//查询todo这个表里的所有代办事项
+		// 查看所有的待办事项
+		v1Group.GET("/todo", func(c *gin.Context) {
+			// 查询todo这个表里的所有数据
 			var todoList []Todo
-			if err = DB.Find(&todoList).Error;err != nil{
-				c.JSON(http.StatusOK, gin.H{
-					"error":err.Error()})
-			}else{
+			if err = DB.Find(&todoList).Error; err!= nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			}else {
 				c.JSON(http.StatusOK, todoList)
 			}
 			
 		})
-		//查看某一个代办事项
-		v1Group.GET("/todo:id",func(c *gin.Context)  {
+		// 查看某一个待办事项
+		v1Group.GET("/todo/:id", func(c *gin.Context) {
 			
 		})
-		//查看所有代办事项
-		v1Group.PUT("/todo:id",func(c *gin.Context)  {
-			id,ok := c.Params.Get("id")
+		// 修改某一个待办事项
+		v1Group.PUT("/todo/:id", func(c *gin.Context) {
+			id, ok := c.Params.Get("id")
 			if !ok {
 				c.JSON(http.StatusOK, gin.H{"error":"Invalid id!"})
 				return
 			}
 			var todo Todo
-			if err = DB.Where("id=?",id).First(&todo).Error;err != nil{
-				c.JSON(http.StatusOK, gin.H{
-					"error":err.Error()})
+			if err = DB.Where("id=?", id).First(&todo).Error; err!=nil{
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
 				return
 			}
 			c.BindJSON(&todo)
@@ -104,8 +113,8 @@ func main()  {
 				c.JSON(http.StatusOK,todo)
 			}
 		})
-		//查看所有代办事项
-		v1Group.DELETE("/todo:id",func(c *gin.Context)  {
+		// 删除某一个待办事项
+		v1Group.DELETE("/todo/:id", func(c *gin.Context) {
 			id, ok := c.Params.Get("id")
 			if !ok{
 				c.JSON(http.StatusOK, gin.H{"error":"Invalid id!"})
